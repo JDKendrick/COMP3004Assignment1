621bb0ec39c3cc71f8847ed59a039b5783d323b9
Jake Kendrick <43183524+JDKendrick@users.noreply.github.com>
2018-09-16 20:25:20 -0400

    test: Fixed up some syntax errors in the test cases

--- src/test/java/common/DeckTester.java
+++ src/test/java/common/DeckTester.java
@@ -1,14 +1,12 @@
 package common;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertThat;
-
 import junit.framework.TestCase;
+import static org.junit.Assert.*;
 
 public class DeckTester extends TestCase{
 	
 	//This method tests to see if the deck object has the correct number of cards (and none are null).
-	public void doesDeckHave52Cards() {		
+	public void testDoesDeckHave52Cards() {		
 		Deck deck = new Deck();
 		
 		assertEquals(52, deck.cards.length);
@@ -16,14 +14,13 @@ public class DeckTester extends TestCase{
 	}
 	
 	//This method tests to see if the deck has been shuffled by appending the contents of the "Cards" array then comparing the strings.
-	public void isDeckShuffled() {
+	public void testIsDeckShuffled() {
 		Deck deck = new Deck();
 		Deck deck2 = new Deck();
 		
-		deck = deck.shuffle();
-		deck2 = deck2.shuffle();
+		deck.shuffle();
+		deck2.shuffle();
 		
-		assertFalse(true, assertArrayEquals(deck.cards, deck2.cards));		
+		assertNotEquals(deck.toString(), deck2.toString());		
 	}
-	
 }
